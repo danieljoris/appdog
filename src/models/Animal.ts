86ffeb2f37a2bd677338da4caac71ec0a6130e90
daniel.joris <danieljoris08@gmail.com>
2020-09-20 17:07:57 -0300

    Added Animal entity.

---
+++ src/models/Animal.ts
@@ -0,0 +1,39 @@
+import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn, CreateDateColumn } from "typeorm";
+
+enum Species {
+  Dog = "Dog",
+  Cat = "Cat"
+}
+
+@Entity("Animals")
+class Animal {
+
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  size: string;
+
+  @Column()
+  sex: string;
+
+  @Column()
+  isCastrated: boolean;
+
+  @Column()
+  approximateAge: number;
+
+  @Column('enum', { enum: Species })
+  specie: Species;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+export default Animal;
