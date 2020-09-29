0826f60fa1c3daf8cdee28ae344711e6078bf39c
Daniel Joris <danieljoris08@gmail.com>
2020-09-28 21:38:52 -0300

    Criado conexão com o banco de dados e alterado arquivo de configurações do bd.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
 
 import routes from './routes';
 
+import './database';
+
 const app = express();
 
-// acho que é isso, né?
 app.use(routes);
 
 app.get('/', (request, response) => {
