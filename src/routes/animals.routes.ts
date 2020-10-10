74b7fc2b7d3c9ad0a506d6e1eeae618a311145ac
Daniel Joris <danieljoris08@gmail.com>
2020-10-10 17:21:34 -0300

    Adicionado serviços e rotas do Animal

--- src/routes/animals.routes.ts
+++ src/routes/animals.routes.ts
@@ -1,11 +1,42 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import Animal from '../models/Animal';
+import CreateAnimalService from '../services/CreateAnimalService';
 
 const animalsRouter = Router();
 
-animalsRouter.get('/', (request, response) => {
+animalsRouter.get('/', async (request, response) => {
+    // listar todos os animais 
+    const animalsRepository = getRepository(Animal)
+
+    const animals = await animalsRepository.find();
+
+    return response.json(animals);
 });
 
-animalsRouter.post('/', (request, response) => {
+animalsRouter.post('/', async (request, response) => {
+    // cadastra um animal
+
+    try {
+        const { name, size, sex, isCastrated, approximateAge, specie } = request.body;
+
+        const createAnimal = new CreateAnimalService();
+
+        const animal = await createAnimal.execute({
+        name, 
+        size, 
+        sex, 
+        isCastrated, 
+        approximateAge, 
+        specie
+        });
+    
+        return response.json(animal);
+    } 
+    catch (error) {
+        console.log(error)
+    }
 });
 
 export default animalsRouter;
--- src/services/CreateAnimalService.ts
+++ src/services/CreateAnimalService.ts
@@ -1,6 +1,33 @@
-import Animal from '../models/Animal';
+import { getRepository } from 'typeorm';
+import Animal, { Species } from '../models/Animal';
+
+interface Request {
+    name: string;
+    size: string;
+    sex: string;
+    isCastrated: boolean;
+    approximateAge: number;
+    specie: Species.Dog | Species.Cat ;
+}
 
 class CreateAnimalService {
-    public async execute(): Promise<Animal> {
+    public async execute({name, size, sex, isCastrated, approximateAge, specie}: Request): Promise<Animal> {
+        
+        const animalRepository = getRepository(Animal);
+        
+        const animal = animalRepository.create({
+            name, 
+            size, 
+            sex, 
+            isCastrated, 
+            approximateAge, 
+            specie
+        });
+
+        await animalRepository.save(animal);
+
+        return animal;
     }
 }
+
+export default CreateAnimalService;
